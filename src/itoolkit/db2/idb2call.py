8c4e3148bbae281888c3a245b04c27a426f4fe93
jwoehr <jwoehr@softwoehr.com>
2023-06-23 13:37:04 -0600

    typehinted idb2call.py

--- src/itoolkit/db2/idb2call.py
+++ src/itoolkit/db2/idb2call.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# -*- coding: utf-8 -*-
 import warnings
 import os
+
 try:
     import ibm_db
     from ibm_db_dbi import connect, Connection
@@ -9,20 +17,9 @@ except ImportError:
 from ..transport.database import DatabaseTransport
 
 
-class iDB2Call(DatabaseTransport): # noqa N801
+class iDB2Call(DatabaseTransport):  # noqa N801
     """
     Transport XMLSERVICE calls over DB2 connection.
-
-    Args:
-      iuid   (str): Database user profile name or database connection
-      ipwd   (str): optional - Database user profile password
-                               -- or --
-                               env var PASSWORD (export PASSWORD=mypass)
-      idb2   (str): optional - Database (WRKRDBDIRE *LOCAL)
-      ictl   (str): optional - XMLSERVICE control ['*here','*sbmjob']
-      ipc    (str): optional - XMLSERVICE route for *sbmjob '/tmp/myunique'
-      isiz   (int): optional - XMLSERVICE expected max XML output size
-      ilib   (str): optional - XMLSERVICE library compiled (default QXMLSERV)
 
     Example:
       from itoolkit.db2.idb2call import *
@@ -34,15 +31,53 @@ class iDB2Call(DatabaseTransport): # noqa N801
     Returns:
        (obj)
     """
-    def __init__(self, iuid=None, ipwd=None, idb2='*LOCAL', ictl='*here *cdata',
-                 ipc='*na', isiz=None, ilib=None):
+
+    def __init__(
+        self,
+        iuid: str = None,
+        ipwd: str = None,
+        idb2: str = "*LOCAL",
+        ictl: str = "*here *cdata",
+        ipc: str = "*na",
+        isiz: str = None,
+        ilib: str = None,
+    ):
+        """
+
+
+        Parameters
+        ----------
+        iuid : str, optional
+            Database user profile name or database connection. The default is None.
+        ipwd : str, optional
+            Database user profile password
+            -- or --
+            env var PASSWORD (export PASSWORD=mypass). The default is None.
+        idb2 : str, optional
+            Database (WRKRDBDIRE *LOCAL). The default is '*LOCAL'.
+        ictl : str, optional
+            XMLSERVICE control ['*here','*sbmjob']. The default is '*here *cdata'.
+        ipc : str, optional
+            XMLSERVICE route for *sbmjob '/tmp/myunique'. The default is '*na'.
+        isiz : str, optional
+            XMLSERVICE expected max XML output size. The default is None.
+        ilib : str, optional
+            XMLSERVICE library compiled (default QXMLSERV). The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
+
         warnings.warn(
             "iDB2Call is deprecated, "
             "use itoolkit.transport.DatabaseTransport instead",
             category=DeprecationWarning,
-            stacklevel=2)
+            stacklevel=2,
+        )
 
-        if hasattr(iuid, 'cursor'):
+        if hasattr(iuid, "cursor"):
             # iuid is a PEP-249 connection object, just store it
             conn = iuid
         elif isinstance(iuid, ibm_db.IBM_DBConnection):
@@ -51,11 +86,11 @@ class iDB2Call(DatabaseTransport): # noqa N801
             conn = Connection(iuid)
         else:
             # user id and password passed, connect using ibm_db_dbi
-            ipwd = ipwd if ipwd else os.getenv('PASSWORD', None)
+            ipwd = ipwd if ipwd else os.getenv("PASSWORD", None)
             conn = connect(database=idb2, user=iuid, password=ipwd)
 
         if ilib is None:
-            ilib = os.getenv('XMLSERVICE', 'QXMLSERV')
+            ilib = os.getenv("XMLSERVICE", "QXMLSERV")
 
         if isiz is not None:
             msg = "isiz is deprecated and ignored"
