d5264658aad4395acb5f4f6223f7a59fce6173bd
jwoehr <jwoehr@softwoehr.com>
2023-06-27 11:40:04 -0600

    typehinting irestcall.py

--- src/itoolkit/rest/irestcall.py
+++ src/itoolkit/rest/irestcall.py
@@ -4,58 +4,90 @@ import os
 from ..transport.http import HttpTransport
 
 
-class iRestCall(HttpTransport): # noqa N801
+class iRestCall(HttpTransport):  # noqa N801
     """
     Transport XMLSERVICE calls over standard HTTP rest.
-
-    Args:
-      iurl   (str): XMLSERVICE url, eg. https://example.com/cgi-bin/xmlcgi.pgm
-      iuid   (str): Database user profile name
-      ipwd   (str): optional - Database user profile password
-                               -- or --
-                               env var PASSWORD (export PASSWORD=mypass)
-      idb2   (str): optional - Database (WRKRDBDIRE *LOCAL)
-      ictl   (str): optional - XMLSERVICE control ['*here','*sbmjob']
-      ipc    (str): optional - XMLSERVICE route for *sbmjob '/tmp/myunique'
-      isiz   (str): optional - XMLSERVICE expected max XML output size
-
-    Example:
-      from itoolkit.rest.irestcall import *
-      itransport = iRestCall(url,user,password)
     """
-    def __init__(self, iurl, iuid, ipwd=None, idb2=0, ictl=0, ipc=0, isiz=0):
+
+    def __init__(
+        self,
+        iurl: str,
+        iuid: str,
+        ipwd: str = None,
+        idb2: str = 0,
+        ictl: str = 0,
+        ipc: str = 0,
+        isiz: str = 0,
+    ):
+        """
+        Example:
+          from itoolkit.rest.irestcall import *
+          itransport = iRestCall(url,user,password)
+
+        Parameters
+        ----------
+        iurl : str
+            XMLSERVICE url, eg. https://example.com/cgi-bin/xmlcgi.pgm.
+        iuid : str
+            Database user profile name.
+        ipwd : str, optional
+            Database user profile password
+            -- or --
+            env var PASSWORD (export PASSWORD=mypass). The default is None.
+        idb2 : str, optional
+            Database (WRKRDBDIRE *LOCAL). The default is 0.
+        ictl : str, optional
+            XMLSERVICE control ['*here','*sbmjob'. The default is 0.
+        ipc : str, optional
+            XMLSERVICE route for *sbmjob '/tmp/myunique'. The default is 0.
+        isiz : str, optional
+            XMLSERVICE expected max XML output size. The default is 0.
+
+        Returns
+        -------
+        None.
+
+        """
         warnings.warn(
-            "iRestCall is deprecated, "
-            "use itoolkit.transport.HttpTransport instead",
+            "iRestCall is deprecated, " "use itoolkit.transport.HttpTransport instead",
             category=DeprecationWarning,
-            stacklevel=2)
+            stacklevel=2,
+        )
 
         if not ictl:
-            ictl = '*here *cdata'
+            ictl = "*here *cdata"
 
         if not ipc:
-            ipc = '*na'
+            ipc = "*na"
 
         if not ipwd:
-            ipwd = os.environ['PASSWORD']
+            ipwd = os.environ["PASSWORD"]
 
         if not idb2:
-            idb2 = '*LOCAL'
+            idb2 = "*LOCAL"
 
         if isiz not in (0, self.OUT_SIZE):
             msg = "isiz is deprecated, changed to {}".format(self.OUT_SIZE)
             warnings.warn(msg, category=DeprecationWarning, stacklevel=2)
 
-        super().__init__(url=iurl, user=iuid, password=ipwd, database=idb2,
-                         ctl=ictl, ipc=ipc)
+        super().__init__(
+            url=iurl, user=iuid, password=ipwd, database=idb2, ctl=ictl, ipc=ipc
+        )
 
-    def call(self, itool):
-        """Call XMLSERVICE with accumulated actions.
-
-        Args:
-          itool: An iToolkit object
-
-        Returns:
-          The XML returned from XMLSERVICE
+    def call(self, itool) -> str:
         """
+        Call XMLSERVICE with accumulated actions.
+
+        Parameters
+        ----------
+        itool : iToolkit
+            An iToolkit object.
+
+        Returns
+        -------
+        str
+            The XML returned from XMLSERVICE.
+
+        """
+
         return super().call(itool)
